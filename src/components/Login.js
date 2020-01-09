37c04da3a6451b4a7aa88c3605385eb226df0535
alonecheer <alonecheer@hotmail.com>
2020-01-09 16:36:36 +0700

    Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,8 @@ import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import 'antd/dist/antd.css'
 import Topbar from './Topbar'
 import Router from 'next/router'
+import React, { useState } from 'react';
+import axios from 'axios';
 const StyledWrapper = styled.div`
   
 .main{
@@ -27,6 +29,16 @@ p{
 }
 `
 const LogIn = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [loginStatus, setLoginStatus] = useState('Not yet logged in');
+
+    const login = async () => {
+        const res = await axios.post('/api/psu', { username, password });
+
+        setLoginStatus(JSON.stringify(res.data))
+    }
     return (
         
         <StyledWrapper>
@@ -34,10 +46,13 @@ const LogIn = () => {
                 <div className="container">
                 <div className="boxlogin">
                     <p>PSU | PASSPORT</p>
-                    <Form.Item ><Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="username" /></Form.Item>
-                    <Form.Item><Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-                    </Form.Item><Button onClick={() => { Router.push('/main') }} type="primary" block>Sign In</Button>
+                    <Form.Item ><Input onChange={e => setUsername(e.target.value)} prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="username" /></Form.Item>
+                    <Form.Item><Input onChange={e => setPassword(e.target.value)} prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                    </Form.Item><Button onClick={login} type="primary" block>Sign In</Button>
                 </div>
+                <div style={{ color: 'blue' }}>
+                {loginStatus}
+            </div>
             </div>
             </div>
         </StyledWrapper>
